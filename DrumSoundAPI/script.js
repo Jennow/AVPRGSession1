fdfe41ff797799f363ca61b762c49795a272e2d2
jenifer.prochnow <jeniferprochnow@web.de>
2018-10-02 12:53:52 +0200

    Projektaufteilung, DrumSoundAPI Aufgabe

---
+++ DrumSoundAPI/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function(){
+  var $drumpads = document.getElementById('drums');
+  var context = new AudioContext();
+
+  soundbuffers = [];
+
+  for(let i = 0; i < $drumpads.children.length; i++){
+    soundbuffers[i] = new Audio("sounds/sound"+(i+1)+".wav");
+    var soundNode = context.createMediaElementSource(soundbuffers[i]);
+    var gainNode = context.createGain();
+
+    gainNode.gain.value = 0.8;
+
+    soundNode.connect(gainNode);
+    gainNode.connect(context.destination);
+    
+    $drumpads.children[i].addEventListener('click', function(e){soundbuffers[i].play()});
+  }
+});
